a679bf9e272dabf4a2c7e58720c014c539d5f32e
Creat6er-Games-Production <57394014+Creat6er-Games-Production@users.noreply.github.com>
2019-12-30 23:46:54 +0300

    5676867uyj

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Prefabs/PlayerSetup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	public Transform client;
+	public Transform me;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (isLocalPlayer) 
+		{
+			client.gameObject.SetActive (true);
+			me.gameObject.SetActive (false);
+		} 
+		else 
+		{
+			client.gameObject.SetActive (false);
+			me.gameObject.SetActive (true);
+		}
+	}
+}
