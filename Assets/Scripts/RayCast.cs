26a44b6d7db48d2f03b00ff0a5abe73c1ecba048
Creat6er-Games-Production <57394014+Creat6er-Games-Production@users.noreply.github.com>
2020-01-01 18:26:30 +0300

    666

---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour {
+
+	public float distance = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.E)) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay (new Vector2 (Screen.width / 2, Screen.height / 2));
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, distance)) 
+			{
+				if (hit.collider.tag == "Door") 
+				{
+					hit.collider.GetComponent<Animator> ().SetTrigger ("Open");
+				} 
+				else 
+				{
+					hit.collider.GetComponent<Animator> ().SetTrigger ("Close");
+				}
+			}
+		}
+	}
+}
